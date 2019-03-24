f7013cc21c8ba9ef5d9aea5dcb89fb9a227eea86
Karina <Karina@DESKTOP-T2O8FM8>
2019-03-23 20:29:06 -0400

    Import CSS styles, adjust menu options and create empty views, work on the Home view calling the news API endpoint

--- API_Usage/Models/EF_Models.cs
+++ API_Usage/Models/EF_Models.cs
@@ -37,4 +37,15 @@ namespace API_Usage.Models
   {
     public Equity[] chart { get; set; }
   }
+
+    public class News
+    {
+        public string datetime { get; set; }
+        public string headline { get; set; }
+        public string source { get; set; }
+        public string url { get; set; }
+        public string summary { get; set; }
+        public string related { get; set; }
+        public string image { get; set; }
+    }
 }
